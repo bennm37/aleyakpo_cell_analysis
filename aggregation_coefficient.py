130d1bfcc211be282b02710dd6a30b98e5f29cc5
Ben Mindlin <bennm37@gmail.com>
2024-12-23 13:21:08 +0000

    AC Boxplot

--- aggregation_coefficient.py
+++ aggregation_coefficient.py
@@ -7,14 +7,18 @@ from scipy.signal import convolve2d
 from PIL import Image
 from esda.moran import Moran
 from libpysal.weights import lat2W
+from napari import Viewer, run
+from scipy.stats import ttest_ind
 import os
+from stacked_bar import get_symbol
+
 
 plt.style.use("ggplot")
 # transparent, red cmap
 transRed = LinearSegmentedColormap.from_list("transRed", [(0, (0, 0, 0, 0)), (1, (1, 0, 0, 1))], N=2)
 
 
-def aggregation_coefficient(data, n_subvolumes=(10, 10), voxel_size=(1, 1), threshold=True):
+def aggregation_coefficient(data, n_subvolumes=(10, 10), voxel_size=(1, 1), threshold=False):
     """Calculates the aggregation coefficient for a 2D image."""
     subvolumes = get_subvolumes(data, n_subvolumes)
     N = np.prod(n_subvolumes)
@@ -36,7 +40,7 @@ def aggregation_coefficient(data, n_subvolumes=(10, 10), voxel_size=(1, 1), thre
     S_max = T * (N + 1) / 2
     S_min = T
     A = (S_max - S) / (S_max - S_min)
-    return A
+    return A, data >= thresh
 
 
 def calculate_morans_I(data):
@@ -179,12 +183,91 @@ def parse_name(name):
             raise Warning("Not in livecyte format")
             return None
         well_no, roi_no, _, sec_no = name.split("_")
+
     else:
         raise Warning("Not an ome.tiff file")
         return None
+
     return well_no, roi_no, sec_no
 
 
+def get_cell_type(well_no, well_dict={"WT": ["A1", "A2", "A3"], "R67K": ["C1", "C2", "C3"]}):
+    for k, v in well_dict.items():
+        if well_no in v:
+            return k
+    return None
+
+
+def get_acs(folder, plot=False):
+    fnames = [f for f in os.listdir("data/Images") if f.endswith(".ome.tiff")]
+    ac_data = {}
+    for f in fnames:
+        well_no, roi_no, sec_no = parse_name(f)
+        print(f"Well: {well_no}, ROI: {roi_no}, Section: {sec_no}")
+        cell_type = get_cell_type(well_no)
+        print(f"Cell Type: {cell_type}")
+        data = imread(f"data/Images/{f}")
+        # find the second phase if it exists
+        nt, nx, ny = data.shape
+        print(f"Data shape: {data.shape}")
+        ac_results = [aggregation_coefficient(data[i], n_subvolumes=(9, 9)) for i in range(nt)]
+        ac = [a[0] for a in ac_results]
+        masks = np.array([a[1] for a in ac_results])
+        ac_data[f] = [cell_type, ac]
+        if plot:
+            viewer = Viewer()
+            viewer.add_image(data)
+            viewer.add_image(masks, colormap=transRed, blending="additive", opacity=0.5)
+            fig, ax = plt.subplots()
+            ax.plot(ac)
+            ax.set(xlabel="Time", ylabel="Aggregation Coefficient")
+            ax.set_title(f"AC vs t for {cell_type}|{well_no}_{roi_no}_{sec_no}")
+            ax.set_ylim(0, 1)
+            plt.show()
+    return ac_data
+
+
+def load_images(folder):
+    """Generator to lazily load and combine images."""
+    fnames = [f for f in os.listdir(folder) if f.endswith(".ome.tiff")]
+    well_nos, roi_nos, sec_nos = zip(*[parse_name(f) for f in fnames])
+    u_fnames = set([(w, r) for w, r in zip(well_nos, roi_nos)])
+    for w, r in u_fnames:
+        m_fnames = [f for f in os.listdir(folder) if f.startswith(f"{w}_{r}")]
+        data = [imread(f"{folder}/{f}") for f in m_fnames]
+        yield m_fnames, data
+
+
 if __name__ == "__main__":
-    ben_data()
-"/Volumes/lab-vincentj/home/users/aleyakb/Cells/2022_10_28 NucView HEK T vs R67K Apoptosis/2022_10_28 NucView HEK T vs R67K Apoptosis - Outputs/Raw Data/2022-10-28_10-39-51/Images/A1_6_FITC_1.ome.tiff"
+    # ac_data = get_acs("data/Images", plot=False)
+    # np.save("data/ac_data.npy", ac_data)
+    ac_data = np.load("data/ac_data.npy", allow_pickle=True).item()
+    wt_as = np.array([np.mean(a) for (ct, a) in ac_data.values() if ct == "WT"])
+    r67k_as = np.array([np.mean(a) for (ct, a) in ac_data.values() if ct == "R67K"])
+    fig, ax = plt.subplots()
+    fig.set_size_inches(3, 3)
+    ax.boxplot([wt_as, r67k_as], labels=["WT", "R67K"], positions=[0, 1])
+    ax.violinplot([wt_as, r67k_as], positions=[0, 1], showmedians=False, showextrema=False)
+    # calculate the p-value using a t-test
+    t, p = ttest_ind(wt_as, r67k_as)
+    ax.set_title("Mean Aggregation Coefficient")
+    ax.legend()
+    bottom, top = ax.get_ylim()
+    y_range = top - bottom
+    bar_height = (y_range * 0.05) + top
+    bar_tips = bar_height - (y_range * 0.02)
+    text_height = bar_height + (y_range * 0.02)
+    ax.plot([0, 0, 1, 1], [bar_tips, bar_height, bar_height, bar_tips], lw=1, c="k")
+    ax.text(0.5, text_height, get_symbol(p), ha="center", va="center")
+    plt.tight_layout()
+    # plt.savefig("media/ac_boxplot.pdf")
+    plt.show()
+    print(f"p-value: {p}")
+    print(f"Number of WT samples: {len(wt_as)}")
+    print(f"Number of R67K samples: {len(r67k_as)}")
+
+    plt.hist(wt_as, bins=20, alpha=0.5, label="WT")
+    plt.hist(r67k_as, bins=20, alpha=0.5, label="R67K")
+    print(np.mean(wt_as), np.mean(r67k_as))
+    print(np.std(wt_as), np.std(r67k_as))
+    plt.show()
--- plot_livecyte.py
+++ plot_livecyte.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os
 
+
 def animate(df):
     unique_frames = sorted(df["Frame"].unique())
     fig, ax = plt.subplots()
