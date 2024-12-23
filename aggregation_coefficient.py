3f2214937cdc34f4368d3590acd9e9c30ffdfeb9
Ben Mindlin <bennm37@gmail.com>
2024-12-23 16:53:59 +0000

    Confluencies

--- aggregation_coefficient.py
+++ aggregation_coefficient.py
@@ -8,12 +8,14 @@ from PIL import Image
 from esda.moran import Moran
 from libpysal.weights import lat2W
 from napari import Viewer, run
-from scipy.stats import ttest_ind
+import pandas as pd
+from scipy.stats import ttest_ind, normaltest, gaussian_kde
 import os
 from stacked_bar import get_symbol
-
-
 plt.style.use("ggplot")
+# get the default blue and red from the ggplot colorcycle
+RED = plt.rcParams["axes.prop_cycle"].by_key()["color"][0]
+BLUE = plt.rcParams["axes.prop_cycle"].by_key()["color"][1]
 # transparent, red cmap
 transRed = LinearSegmentedColormap.from_list("transRed", [(0, (0, 0, 0, 0)), (1, (1, 0, 0, 1))], N=2)
 
@@ -199,25 +201,24 @@ def get_cell_type(well_no, well_dict={"WT": ["A1", "A2", "A3"], "R67K": ["C1", "
 
 
 def get_acs(folder, plot=False):
-    fnames = [f for f in os.listdir("data/Images") if f.endswith(".ome.tiff")]
     ac_data = {}
-    for f in fnames:
+    for f, data in load_images(folder):
         well_no, roi_no, sec_no = parse_name(f)
         print(f"Well: {well_no}, ROI: {roi_no}, Section: {sec_no}")
         cell_type = get_cell_type(well_no)
         print(f"Cell Type: {cell_type}")
-        data = imread(f"data/Images/{f}")
         # find the second phase if it exists
         nt, nx, ny = data.shape
         print(f"Data shape: {data.shape}")
         ac_results = [aggregation_coefficient(data[i], n_subvolumes=(9, 9)) for i in range(nt)]
+        confluencies = [np.sum(m) / (nx * ny) for a, m in ac_results]
         ac = [a[0] for a in ac_results]
         masks = np.array([a[1] for a in ac_results])
-        ac_data[f] = [cell_type, ac]
+        ac_data[f] = [cell_type, ac, confluencies]
         if plot:
             viewer = Viewer()
             viewer.add_image(data)
-            viewer.add_image(masks, colormap=transRed, blending="additive", opacity=0.5)
+            viewer.add_labels(masks)
             fig, ax = plt.subplots()
             ax.plot(ac)
             ax.set(xlabel="Time", ylabel="Aggregation Coefficient")
@@ -233,41 +234,127 @@ def load_images(folder):
     well_nos, roi_nos, sec_nos = zip(*[parse_name(f) for f in fnames])
     u_fnames = set([(w, r) for w, r in zip(well_nos, roi_nos)])
     for w, r in u_fnames:
-        m_fnames = [f for f in os.listdir(folder) if f.startswith(f"{w}_{r}")]
-        data = [imread(f"{folder}/{f}") for f in m_fnames]
-        yield m_fnames, data
+        m_fnames = sorted([f for f in fnames if f.startswith(f"{w}_{r}")])
+        data = np.concatenate([imread(f"{folder}/{f}") for f in m_fnames], axis=0)
+        f_name = f"{w}_{r}_Phase_Combined.ome.tiff"
+        yield f_name, data
 
 
-if __name__ == "__main__":
-    # ac_data = get_acs("data/Images", plot=False)
-    # np.save("data/ac_data.npy", ac_data)
-    ac_data = np.load("data/ac_data.npy", allow_pickle=True).item()
-    wt_as = np.array([np.mean(a) for (ct, a) in ac_data.values() if ct == "WT"])
-    r67k_as = np.array([np.mean(a) for (ct, a) in ac_data.values() if ct == "R67K"])
+def box_violin_plot(wt_as, r67k_as):
     fig, ax = plt.subplots()
-    fig.set_size_inches(3, 3)
+    fig.set_size_inches(4, 3)
     ax.boxplot([wt_as, r67k_as], labels=["WT", "R67K"], positions=[0, 1])
     ax.violinplot([wt_as, r67k_as], positions=[0, 1], showmedians=False, showextrema=False)
+    # check normality
+    z_wt, p_wt = normaltest(wt_as, nan_policy="omit")
+    z_r67k, p_r67k = normaltest(r67k_as, nan_policy="omit")
+
     # calculate the p-value using a t-test
-    t, p = ttest_ind(wt_as, r67k_as)
+    t, p_ttest = ttest_ind(wt_as, r67k_as)
     ax.set_title("Mean Aggregation Coefficient")
-    ax.legend()
     bottom, top = ax.get_ylim()
     y_range = top - bottom
     bar_height = (y_range * 0.05) + top
     bar_tips = bar_height - (y_range * 0.02)
     text_height = bar_height + (y_range * 0.02)
     ax.plot([0, 0, 1, 1], [bar_tips, bar_height, bar_height, bar_tips], lw=1, c="k")
-    ax.text(0.5, text_height, get_symbol(p), ha="center", va="center")
+    ax.text(0.5, text_height, get_symbol(p_ttest), ha="center", va="center")
     plt.tight_layout()
-    # plt.savefig("media/ac_boxplot.pdf")
+    plt.savefig("media/ac_boxplot.pdf")
     plt.show()
-    print(f"p-value: {p}")
-    print(f"Number of WT samples: {len(wt_as)}")
-    print(f"Number of R67K samples: {len(r67k_as)}")
+    stats_log = {
+        "Number of WT samples": len(wt_as),
+        "Number of R67K samples": len(r67k_as),
+        "p-value": p_ttest,
+        "Normality test WT": p_wt,
+        "Normality test R67K": p_r67k,
+    }
+    print(f"{k}: {v} \n" for k, v in stats_log.items())
+    pd.DataFrame(stats_log, index=[0]).to_csv("media/ac_stats.csv", index=False)
 
-    plt.hist(wt_as, bins=20, alpha=0.5, label="WT")
-    plt.hist(r67k_as, bins=20, alpha=0.5, label="R67K")
-    print(np.mean(wt_as), np.mean(r67k_as))
-    print(np.std(wt_as), np.std(r67k_as))
+
+def get_array(ac_data):
+    for ct in ["WT", "R67K"]:
+        a_s, cons = zip(*[(a, con) for (c, a, con) in ac_data.values() if c == ct])
+        max_len = max([len(a) for a in a_s])
+        a_s_padded = np.array([np.pad(a, max_len - len(a), mode="constant", constant_values=(np.nan,)) for a in a_s])
+        cons_padded = np.array([np.pad(c, max_len - len(c), mode="constant", constant_values=(np.nan,)) for c in cons])
+        yield a_s_padded, cons_padded
+
+
+def time_plot(ac_data):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(4, 3)
+    (wt_as, _), (r67k_as, _) = get_array(ac_data)
+    t = np.arange(wt_as.shape[1])
+    for a_s, name, color in zip([wt_as, r67k_as], ["WT", "R67K"], [BLUE, RED]):
+        mu, sigma = np.nanmean(a_s, axis=0), np.nanstd(a_s, axis=0)
+        ax.plot(t, mu, label=name, color=color)
+        ax.fill_between(t, mu - sigma, mu + sigma, alpha=0.3, color=color)
+    ax.set_title("Aggregation Coefficient over Time")
+    ax.set(xlabel="Time", ylabel="Aggregation Coefficient")
+    ax.legend()
+    plt.tight_layout()
+    plt.savefig("media/ac_time_plot.pdf")
     plt.show()
+
+
+def aggregation_confluency_scatter_plot(ac_data):
+    fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
+    fig.set_size_inches(6, 3)
+    (wt_as, wt_cons), (r67k_as, r67k_cons) = get_array(ac_data)
+    for i, (a_s, cons, name, cmap) in enumerate(zip([wt_as, r67k_as], [wt_cons, r67k_cons], ["WT", "R67K"], ["Blues", "Reds"])):
+        xy = np.vstack([cons.flatten(), a_s.flatten()])
+        z = gaussian_kde(xy)(xy)
+        ax[i].scatter(cons.flatten(), a_s.flatten(), c=z, s=10, cmap=cmap)
+        ax[i].set(xlabel="Confluency", ylabel="Aggregation Coefficient")
+        ax[i].set_title(name)
+    fig.suptitle = "Aggregation Coefficient vs Confluency"
+    # ax.legend()
+    plt.tight_layout()
+    plt.savefig("media/ac_confluency_plot.pdf")
+    plt.show()
+
+
+def aggregation_confluency_shaded_error_plot(ac_data):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(3, 3)
+    (wt_as, wt_cons), (r67k_as, r67k_cons) = get_array(ac_data)
+    na, nc = 100, 100
+    for i, (a_s, cons, name, cmap) in enumerate(zip([wt_as, r67k_as], [wt_cons, r67k_cons], ["WT", "R67K"], ["Blues", "Reds"])):
+        xy = np.vstack([cons.flatten(), a_s.flatten()])
+        density = gaussian_kde(xy)
+        a, c = np.linspace(0, 1, na), np.linspace(0, 1, nc)
+        A, C = np.meshgrid(a, c)
+        Z = density(np.vstack([C.flatten(), A.flatten()]))
+        Z = Z.reshape((na, nc))
+        # normalize Z by confluency
+        Z /= np.trapz(y=Z, x=c, axis=0)
+        ax.pcolor(C, A, Z, cmap=cmap)
+        # ax.contourf(C, A, Z, cmap=cmap)
+    fig.suptitle = "Aggregation Coefficient vs Confluency"
+    # ax.legend()
+    plt.tight_layout()
+    plt.savefig("media/ac_confluency_shaded_error_plot.pdf")
+    plt.show()
+
+
+def show_videos(folder):
+    for f, data in load_images(folder):
+        viewer = Viewer()
+        viewer.add_image(data)
+        # viewer.window.add_dock_widget("text", name="Title")
+        viewer.window.title = f
+        run()
+
+
+if __name__ == "__main__":
+    # ac_data = get_acs("data/Images", plot=False)
+    # np.save("data/ac_data.npy", ac_data)
+    ac_data = np.load("data/ac_data.npy", allow_pickle=True).item()
+    wt_as = np.array([np.mean(a) for (ct, a, _) in ac_data.values() if ct == "WT"])
+    r67k_as = np.array([np.mean(a) for (ct, a, _) in ac_data.values() if ct == "R67K"])
+    box_violin_plot(wt_as, r67k_as)
+    time_plot(ac_data)
+    aggregation_confluency_scatter_plot(ac_data)
+    # aggregation_confluency_shaded_error_plot(ac_data)
